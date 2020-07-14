40422b9ecc3597aa0903c198241cb37905a72bcd
Pablo Santos <pablo.trybe@gmail.com>
2020-07-13 21:48:49 -0300

    Adicionado os exercícios de 2 a 5

---
+++ JavaScript/4-1/exe_8.js
@@ -0,0 +1,8 @@
+let a = 0;
+if (a < 0){
+  console.log ("Negative");
+} else if (a > 0) {
+  console.log ("Positive")
+} else {
+  console.log ("Zero");
+}
---
+++ JavaScript/4-1/exe_9.js
@@ -0,0 +1,10 @@
+let ang1 = 90;
+let ang2 = 45;
+let ang3 = 45;
+let sum = ang1 + ang2 + ang3;
+
+if (sum === 180) {
+  console.log(true);
+} else {
+  console.log (false);
+}
