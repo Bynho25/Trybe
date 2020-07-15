382f9f0f269b92202a23f4c5c93316a59b7e90d4
Pablo Santos <pablo.trybe@gmail.com>
2020-07-14 21:34:13 -0300

    Adicionando os exercicios do bloco 4.2 de javascript, for e arrays

---
+++ JavaScript/4-2/ex7.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lower = 100;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers [i] < lower) {
+    lower = numbers[i];
+  }
+}
+console.log (lower);
