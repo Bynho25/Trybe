e3c652997cc0f8188741399146aadd8d3320290e
Pablo Santos <pablo.trybe@gmail.com>
2020-07-24 23:22:47 -0300

    Exercicio quase finalizado

--- JavaScript/5-4/script.js
+++ JavaScript/5-4/script.js
@@ -1,12 +1,16 @@
-window.onload = function() {
-  if(localStorage.length === 0) {
+window.onload = function () {
+  if (localStorage.length === 0) {
     document.body.style.backgroundColor = 'lightgray';
     document.body.style.color = 'black';
     document.body.style.fontSize = '12px';
+    document.body.style.lineHeight = '1.5';
+    document.body.style.fontFamily = 'comic-sans';
   } else {
     document.body.style.backgroundColor = localStorage.getItem('background-color')
     document.body.style.color = localStorage.getItem('text-color');
     document.body.style.fontSize = localStorage.getItem('font-size');
+    document.body.style.lineHeight = localStorage.getItem('line-height');
+    document.body.style.fontFamily = localStorage.getItem('font-family');
   }
 }
 /*Alterar a cor do background da página*/
@@ -14,9 +18,11 @@ var bkgColor = document.getElementById('bkgColor').value;
 var body = document.querySelector('body');
 var btnColor = document.getElementById('btnColor');
 btnColor.addEventListener('click', changeBackgroundColor);
+
 function changeBackgroundColor() {
   body.style.backgroundColor = bkgColor;
   localStorage.setItem('background-color', bkgColor);
+  //document.getElementById('bkgColor').value='';
 }
 /*Alterar a cor do texto*/
 var txtColor = document.getElementById('txtColor').value;
@@ -33,4 +39,30 @@ btnSizeFont.addEventListener('click', changeSizeFont);
 function changeSizeFont() {
   document.body.style.fontSize = sizeFont;
   localStorage.setItem('font-size', sizeFont);
+}
+/*Alterar espaçamento entre linhas*/
+var spaceLine = document.getElementById('spaceLine').value;
+var btnSpaceLine = document.getElementById('btnSpaceLine');
+btnSpaceLine.addEventListener('click', changeSpaceLine);
+function changeSpaceLine() {
+  document.body.style.lineHeight = spaceLine;
+  localStorage.setItem('line-height', spaceLine);
+}
+/*Alterar a fonte*/
+var font = document.getElementById('changeFont').value;
+var btnChangeFont = document.getElementById('btnChangeFont');
+btnChangeFont.addEventListener('click', changeFont);
+function changeFont() {
+  document.body.style.fontFamily = font;
+  localStorage.setItem('font-family', font);
+}
+/*Limpar predefinições*/
+var btnClear = document.getElementById('btnClear');
+btnClear.addEventListener('click', clear);
+function clear() {
+  document.body.style.backgroundColor = 'lightgray';
+  document.body.style.color = 'black';
+  document.body.style.fontSize = '12px';
+  document.body.style.lineHeight = '1.5';
+  document.body.style.fontFamily = 'comic-sans';
 }
