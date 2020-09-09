adcae17c50ec675eab5887a7dc07efc1f932d06b
Pablo Santos <pablo.trybe@gmail.com>
2020-09-09 18:49:23 -0300

    Hello world in React

---
+++ React/testando-meu-computador/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import './Component.js';
+import Component from './Component.js';
+
+function App() {
+  return <Component/>
+}
+
+export default App;
---
+++ React/testando-meu-computador/src/Component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Component extends React.Component{
+  render() {
+    return <div>
+      <h1>Hello world!</h1>
+      <h1>Pablo Santos</h1>
+      <p>
+        Oi eu sou o Goku!
+      </p>
+    </div>
+  }
+};
+
+export default Component;
